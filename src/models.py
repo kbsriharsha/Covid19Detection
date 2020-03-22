8a0a3d861686a4b5ed12bd9798c698ecde143e58
Bharat Sri Harsha Karpurapu <kbsriharsha@Bharats-MBP.fios-router.home>
2020-03-22 11:06:36 -0400

    Adding mobilenet and xception models

---
+++ src/models.py
@@ -0,0 +1,147 @@
+# !/Users/kbsriharsha/anaconda3/bin/python
+# coding: utf-8
+# @author: Bharat Sri Harsha karpurapu
+
+"""
+This program provides all the different models used in the training purposes
+"""
+
+# Importing all the necessary libraies and functions
+import numpy as np
+import pandas as pd
+from keras.layers import Activation, Convolution2D, Dropout, Conv2D, Dense
+from keras.layers import AveragePooling2D, BatchNormalization
+from keras.layers import GlobalAveragePooling2D
+from keras.models import Sequential
+from keras.layers import Flatten
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import MaxPooling2D
+from keras.layers import SeparableConv2D
+from keras import layers
+from keras.regularizers import l2
+
+
+def mini_xception(input_shape, num_classes, regularization = l2(0.01)):
+
+    """
+    This function architects the mini_xception model network. This is the best
+    performing model in the facial emotion analysis
+
+    input_shape: input shape of the image
+    num_classes: number of classes in the output
+
+    return: Returns the fully architected model
+    """
+    # base
+    img_input = Input(input_shape)
+    x = Conv2D(8, (3, 3), strides=(1, 1), kernel_regularizer=regularization,
+               use_bias=False)(img_input)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(8, (3, 3), strides=(1, 1), kernel_regularizer=regularization,
+               use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    # module 1
+    residual = Conv2D(16, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(16, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(16, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+
+    # module 2
+    residual = Conv2D(32, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(32, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(32, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+
+    # module 3
+    residual = Conv2D(64, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(64, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(64, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+
+    # module 4
+    residual = Conv2D(128, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(128, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(128, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+
+    x = Conv2D(num_classes, (3, 3),
+               # kernel_regularizer=regularization,
+               padding='same')(x)
+    x = GlobalAveragePooling2D()(x)
+    output = Activation('softmax', name='predictions')(x)
+
+    model = Model(img_input, output)
+    return model
+
+
+def mobilenet(input_shape = (224,224,3), num_classes):
+    """
+    This function creates the mobilenet model based on the transfer learning
+    concepts
+
+    input_shape: input shape of the image
+    num_classes: number of classes in the output
+
+    return: returns the mobile net model
+    """
+    img_input = Input(input_shape)
+
+    model = keras.applications.mobilenet.MobileNet(include_top=False, weights= 'imagenet', input_shape = input_shape)
+    x = keras.layers.GlobalAveragePooling2D()(model.output)
+    new_output = keras.layers.Dense(num_classes, activation='softmax')(new_output)
+    out_put = keras.engine.training.Model(model.inputs, new_output)
+
+    model = Model(img_input, output)
+    return model
